9d1db448993a5beffa5c48e3c1841d17ad9eb2f5
Phornsri <69131130+Phornsri@users.noreply.github.com>
2020-08-05 21:31:42 -0400

    Update GitFun.java

--- src/Utility/GitFun.java
+++ src/Utility/GitFun.java
@@ -2,6 +2,8 @@ package Utility;
 
 public class GitFun {
     public static void main(String[] args) {
+        
+        // how to git pull?
 
         System.out.println("Eveything is fun");
     }
