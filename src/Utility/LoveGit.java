784d11ce8d3f1ae8d69ea85a5eb69a6f15890a9d
Phornsri <69131130+Phornsri@users.noreply.github.com>
2020-08-05 21:36:21 -0400

    Update LoveGit.java

--- src/Utility/LoveGit.java
+++ src/Utility/LoveGit.java
@@ -2,6 +2,7 @@ package Utility;
 
 public class LoveGit {
     public static void main(String[] args) {
+        // yes, it's fun indeed! ._.
         System.out.println("Hello Git's club!!");
     }
 }
